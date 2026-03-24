6fb4c624db0679eba62aea8f3b3243de649b286a
Esteban Lopez <esteban505r@gmail.com>
2026-03-24 09:10:11 -0500

    Added Oter logic

--- modules/users.nix
+++ modules/users.nix
@@ -3,7 +3,9 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Load nvm in interactive shells (install with: n). Silence "bash: hash: hashing disabled" (nvm uses hash; NixOS has it disabled).
+  # Load nvm in interactive shells.
+  # Install: curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+  # Silence "bash: hash: hashing disabled" (nvm uses hash; NixOS has it disabled).
   environment.interactiveShellInit = ''
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 2>/dev/null
